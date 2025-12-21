7b901bb483c8a49422554005d9ba08c0cb9fdd75
Michael Rodrigues <rodriguesm86@hotmail.com>
2025-12-21 18:01:47 -0300

    feat: Add a new chat filter widget with a configurable wordlist.

---
+++ chat_filter_wordlist_data.lua
@@ -0,0 +1,12 @@
+-- List of words to be filtered
+-- This file works as a simple data container
+-- Returns a table of strings
+
+return {
+    "badword",
+    "slur",
+    "insult",
+    "troll",
+    "spam",
+    -- Add more words here
+}
